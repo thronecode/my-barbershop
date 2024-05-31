293fc2d5cb3848cdcc82d3cf4532c1be7f5fde07
Reinaldo Leite <reinasdev+github@gmail.com>
2024-05-31 11:22:45 -0300

    Add middleware

--- backend/interface/admin/router.go
+++ backend/interface/admin/router.go
@@ -1,11 +1,13 @@
 package admin
 
 import (
+	"backend/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine) {
 	admin := router.Group("/api/admin")
+	admin.Use(middleware.AuthMiddleware())
 	{
 		admin.POST("/", add)
 		admin.GET("/", list)
